e07d05baf6585195730e3a017c00bfd17814ee5f
Govind Yadav <technoedits711@gmail.com>
2026-07-01 22:26:50 +0530

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -21,14 +21,19 @@
 import User from "../models/user.js";
 
 export const protect = async (req, res, next) => {
-    const { userId } = req.auth();
-    if (!userId) {
-        return res.json({ success: false, message: "not authentication" });
+    try {
+        const { userId } = req.auth();
+        if (!userId) {
+            return res.json({ success: false, message: "not authentication" });
+        }
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error("Protect middleware error:", error.message);
+        return res.status(500).json({ success: false, message: error.message });
     }
-    const user = await User.findById(userId);
-    if (!user) {
-        return res.json({ success: false, message: "User not found in DB" });
-    }
-    req.user = user;
-    next();
 }
